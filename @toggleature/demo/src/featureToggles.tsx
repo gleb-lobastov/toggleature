b94be8958e14fb46e1b2b8716eb371490a22c19b
gleb.lobastov <gleb.lobastov@gmail.com>
2021-12-02 01:36:43 +0300

    feat: add lookup config for demo and allow custom local storage key (for iframe mode)

--- @toggleature/demo/src/featureToggles.tsx
+++ @toggleature/demo/src/featureToggles.tsx
@@ -4,9 +4,19 @@ import { FeatureTogglesState } from "@toggleature/core-bus";
 
 export type Features = "demoFeature" | "enabledByDefaultFeature";
 
+const DEFAULT_LOCAL_STORAGE_KEY = "@@toggleature:browser:state";
+const DEFAULT_SEARCH_PARAMS_KEY = "@@toggleature:browser:state";
+
+const params = new URL(document.location.href).searchParams;
+
 const { useFeatureToggle, FeatureToggleProvider, FeatureToggle } =
   createFeatureToggles<Features>(
-    featuresConfig as FeatureTogglesState<Features>
+    featuresConfig as FeatureTogglesState<Features>,
+    {
+      localStorageKey:
+        params.get("localStorageKey") ?? DEFAULT_LOCAL_STORAGE_KEY,
+      searchParamsKey: DEFAULT_SEARCH_PARAMS_KEY,
+    }
   );
 
 export { useFeatureToggle, FeatureToggleProvider, FeatureToggle };
